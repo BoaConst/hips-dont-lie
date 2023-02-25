463b6a4343ab8f83da78d7d090bbc2c8df82fe28
BoaConst <suumilroy120@gmail.com>
2023-02-25 14:46:01 -0500

    updated with chatbot widget

---
+++ src/components/ChatBotWidget/README.md
@@ -0,0 +1,33 @@
+# ChatBotWidget
+
+<!-- STORY -->
+
+## Introduction
+
+ChatBotWidget is an easy-to-use component.
+
+## Usage
+
+```javascript
+import { ChatBotWidget } from 'some-package-name';
+```
+
+## Example use
+
+```javascript
+const myPage = props => {
+  return (
+    <main>
+      <ChatBotWidget />
+    </main>
+  );
+};
+```
+
+## Properties
+
+- `className` - is the class name of the component
+
+| propName  | propType | defaultValue | isRequired |
+| --------- | -------- | ------------ | ---------- |
+| className | string   | -            | -          |
--- src/components/HealthInformationLanding/HealthInformationLanding.js
+++ src/components/HealthInformationLanding/HealthInformationLanding.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Content, Wrapper } from '../../HealthInformationLanding.styles';
 
+import ChatbotWidget from '../ChatBotWidget/ChatBotWidget';
+import './HealthInformationLanding.css';
 const HealthInformationLanding = () => {
+
+	
 	return (
-		<div>This is a component called HealthInformationLanding.</div>
-	)
+		// <div className="container"></div>
+		<Wrapper className="d-grid gap-3 pt-3 mb-4 px-4 bg-light border rounded-3">
+            <Content className="row">
+			<div className="tile red">Tile 1</div>
+			<div className="tile green">Tile 2</div>
+			<div className="tile blue">Tile 3</div>
+			<div className="tile red">Tile 4</div>
+			<div className="tile green">Tile 5</div>
+			<div className="tile blue">Tile 6</div>
+
+			<textarea style={{height:"24px", width:"80%", marginLeft:"10%" }}></textarea>
+			</Content>
+		</Wrapper>
+	);
 } 
 
 export default HealthInformationLanding;
