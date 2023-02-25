7de332cb68440c8d6d0b131f1547a7f534430c9a
BoaConst <suumilroy120@gmail.com>
2023-02-25 16:09:49 -0500

    capturing text in textbox

--- src/components/HealthInformationLanding/HealthInformationLanding.js
+++ src/components/HealthInformationLanding/HealthInformationLanding.js
@@ -1,25 +1,49 @@
 import React from 'react';
 import { Content, Wrapper } from '../../HealthInformationLanding.styles';
-
-import ChatbotWidget from '../ChatBotWidget/ChatBotWidget';
+import { FaSearch } from "react-icons/fa";
 import './HealthInformationLanding.css';
+
+
 const HealthInformationLanding = () => {
 
-	
-	return (
-		// <div className="container"></div>
-		<Wrapper className="d-grid gap-3 pt-3 mb-4 px-4 bg-light border rounded-3">
-            <Content className="row">
-			<div className="tile red">Tile 1</div>
-			<div className="tile green">Tile 2</div>
-			<div className="tile blue">Tile 3</div>
-			<div className="tile red">Tile 4</div>
-			<div className="tile green">Tile 5</div>
-			<div className="tile blue">Tile 6</div>
+	const handleChange = () => {
+		let element = document.getElementById("query");
 
-			<textarea style={{height:"24px", width:"80%", marginLeft:"10%" }}></textarea>
+		console.log("Value: %s", element.value)
+	};
+
+	return (
+		<div >
+			<Wrapper className="d-grid gap-3 pt-3 mb-4 px-4 bg-light border rounded-3">
+            <Content className="row" style={{backgroundColor:"#D3D3D3"}}>
+				<div style={{display: "inherit", marginLeft: "15%"}}>
+					<div className="tile tileColor">Tile 1</div>
+					<div className="tile tileColor">Tile 2</div>
+					<div className="tile tileColor">Tile 3</div>
+				</div>
+				<div style={{display: "inherit", marginLeft: "15%", marginTop: "2%"}}>
+					<div className="tile tileColor">Tile 4</div>
+					<div className="tile tileColor">Tile 5</div>
+					<div className="tile tileColor" onClick={handleChange}>Tile 6</div>
+				</div>
+				<div style = {{display : "flex"}}>
+					<textarea style={{ 
+						height:"40px", 
+						width:"85%", 
+						marginTop: "3%", 
+						marginLeft:"8%", 
+						backgroundColor:"#5A5A5A"}}
+						id="query">
+					</textarea>
+					<FaSearch style={{ 
+						height:"40px", 
+						marginTop: "3%"}}
+						onClick={handleChange}
+					></FaSearch>
+				</div>
 			</Content>
 		</Wrapper>
+		</div>
 	);
 } 
 
