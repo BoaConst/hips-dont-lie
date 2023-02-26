f422cec4ce6fc6df5fcac335415da1acb603ea7e
BoaConst <suumilroy120@gmail.com>
2023-02-26 09:57:54 -0500

    tile data

--- src/components/HealthInformationLanding/HealthInformationLanding.js
+++ src/components/HealthInformationLanding/HealthInformationLanding.js
@@ -4,20 +4,20 @@ import React, { useState } from 'react';
 import { Content, Wrapper } from '../../HealthInformationLanding.styles';
 import './HealthInformationLanding.css';
 import { useEffect } from 'react';
+import { COMMON_AILMETS } from '../../constants/COMMON_AILMENTS';
+import { AILMENT_STRING } from '../../constants/AILMENT_STRING';
 
 
 const HealthInformationLanding = (props) => {
-
 	const tiles = document.querySelectorAll('.tile');
-	const [showTable, setShowTable] = useState(props.showTable);
-	const [query, setQuery] = useState("");
-	const [data, setData] = useState("");
-
+	const tileNames = Object.keys(COMMON_AILMETS);
+	
 	tiles.forEach(tile => {
 		let isClicked = false;
 		tile.addEventListener('click', () => {
 			if (!isClicked) {
 				// Display information about the clicked tile
+				console.log("here")
 				tile.textContent = 'Clicked!';
 				tile.style.backgroundColor = 'blue';
 				isClicked = true;
@@ -30,33 +30,41 @@ const HealthInformationLanding = (props) => {
 		});
 	});
 
+	const [showTable, setShowTable] = useState(props.showTable);
+	const [query, setQuery] = useState("");
+	const [data, setData] = useState("");
+
 	useEffect(() => {
 		const fetchData = async () => {
-			const apiUrl = 'https://api.openai.com/v1/completions';
+			if (query) {
+				const apiUrl = 'https://api.openai.com/v1/completions';
 
-			const postData = {
-				"model": "text-davinci-003",
-				"prompt": query + ", Provide answer in medical/health context.",
-				"temperature": 0,
-				"max_tokens": 200
-			};
+				const postData = {
+					"model": "text-davinci-003",
+					"prompt": query + ", Provide answer in medical/health context.",
+					"temperature": 0,
+					"max_tokens": 200
+				};
 
-			console.log(postData.prompt)
+				console.log(postData.prompt)
 
-			try {
-				const response = await fetch(apiUrl, {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-						'Authorization': 'Bearer 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'
-					},
-					body: JSON.stringify(postData)
-				});
-				let result = await response.json();
-				setData(result.choices[0].text)
-			} catch (error) {
-				console.error(error);
+				try {
+					const response = await fetch(apiUrl, {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',
+							'Authorization': 'Bearer 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'
+						},
+						body: JSON.stringify(postData)
+					});
+					let result = await response.json();
+					setData(result.choices[0].text);
+					console.log(data);
+				} catch (error) {
+					console.error(error);
+				}
 			}
+
 		};
 
 		fetchData();
@@ -65,10 +73,13 @@ const HealthInformationLanding = (props) => {
 	const handleChange = () => {
 		const element = document.getElementById("query");
 		setQuery(element.value);
-		if (query) {
+		if (query && data) {
 			toggleTable();
-			console.log(data);
 		}
+	};
+
+	const updateResults = () => {
+
 	};
 
 	const toggleTable = () => {
@@ -76,51 +87,29 @@ const HealthInformationLanding = (props) => {
 	};
 
 	return (
-
-		<Wrapper className="d-grid gap-3 pt-3 mb-4 px-4 bg-light border rounded-3">
 			<Content className="row" style={{ backgroundColor: "#D3D3D3" }}>
 				{!showTable && (<div>
-					<div style={{ display: "flex", marginLeft: "15%", marginTop: "1%" }}>
-						<div className="tile tileColor" value>Tile 1</div>
-						<div className="tile tileColor">Tile 2</div>
-						<div className="tile tileColor">Tile 3</div>
+					<div style={{ display: "flex", marginLeft:"15%", marginTop: "1%" }}>
+						<div className="tile tileColor tileCPR">{AILMENT_STRING.CPR}</div>
+						<div className="tile tileColor tileHeatStroke">{AILMENT_STRING.HEAT_STROKE}</div>
+						<div className="tile tileColor tileMinorBurns">{AILMENT_STRING.MINOR_BURNS}</div>
 					</div>
-					<div style={{ display: "flex", marginLeft: "15%", marginTop: "2%" }}>
-						<div className="tile tileColor">Tile 4</div>
-						<div className="tile tileColor">Tile 5</div>
-						<div className="tile tileColor">Tile 6</div>
+					<div style={{ display: "flex", marginLeft:"15%", marginTop: "2%" }}>
+						<div className="tile tileColor tileMinorCuts">{AILMENT_STRING.MINOR_CUTS}</div>
+						<div className="tile tileColor tileInsulin">{AILMENT_STRING.DIABETES}</div>
+						<div className="tile tileColor tileAsthmaAttack">{AILMENT_STRING.ASTHMA_ATTACK}</div>
 					</div>
 				</div>)}
-				<div>
-					{showTable && (
-						<table>
-							<thead>
-								<tr>
-									<th>Header 1</th>
-									<th>Header 2</th>
-									<th>Header 3</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>Cell 1</td>
-									<td>Cell 2</td>
-									<td>Cell 3</td>
-								</tr>
-								<tr>
-									<td>Cell 4</td>
-									<td>Cell 5</td>
-									<td>Cell 6</td>
-								</tr>
-							</tbody>
-						</table>
-					)}
-				</div>
 				<div className="search-bar">
-					<input
+					{showTable && (<div>
+						<button type="submit"
+							onClick={handleChange}>
+							<FontAwesomeIcon icon={faSearch} />
+						</button>
+						<input
 						style={{
 							height: "50px",
-							width: "85%",
+							width: "80%",
 							marginTop: "3%",
 							marginBottom: "3%",
 							marginLeft: "8%",
@@ -129,13 +118,39 @@ const HealthInformationLanding = (props) => {
 						id="query"
 						type="text"
 					/>
+
+					<button type="submit"
+						onClick={updateResults}>
+						<FontAwesomeIcon icon={faSearch} />
+					</button>
+					</div>)}
+					{!showTable && (<div style={{textAlign:"center"}}>
+						<input
+						style={{
+							height: "50px",
+							width: "70%",
+							marginTop: "2%",
+							marginBottom: "3%",
+							backgroundColor: "#5A5A5A"
+						}}
+						id="query"
+						type="text"
+					/>
+
 					<button type="submit"
 						onClick={handleChange}>
 						<FontAwesomeIcon icon={faSearch} />
 					</button>
+					</div>)}
+					
 				</div>
+				{showTable && (<div>
+					<span className="tile tileColor" style={{ marginLeft: "6%", marginBottom: "3%", width: "90%", height: "fit-content" }}><p style={{ whiteSpace: "pre-line" }}>{data}</p></span>
+				</div>
+
+				)}
+
 			</Content>
-		</Wrapper>
 
 	);
 }
