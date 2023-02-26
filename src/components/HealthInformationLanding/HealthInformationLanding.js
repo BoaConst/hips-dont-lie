c227bf0967640a873569a8eb693655688607e29e
AshokKakunuri <ashokreddykakunuri1999@gmail.com>
2023-02-26 00:01:02 -0500

    Googletranslate API intergrated

--- src/components/HealthInformationLanding/HealthInformationLanding.js
+++ src/components/HealthInformationLanding/HealthInformationLanding.js
@@ -12,6 +12,9 @@ const HealthInformationLanding = (props) => {
 	const [showTable, setShowTable] = useState(props.showTable);
 	const [query, setQuery] = useState("");
 	const [data, setData] = useState("");
+	const [source_language, setSourceLang] = useState("");
+	const [english_text, setEnglishData] = useState("");
+	const [translated_data, setTData] = useState("");
 
 	tiles.forEach(tile => {
 		let isClicked = false;
@@ -33,17 +36,58 @@ const HealthInformationLanding = (props) => {
 	useEffect(() => {
 		const fetchData = async () => {
 			const apiUrl = 'https://api.openai.com/v1/completions';
-
-			const postData = {
-				"model": "text-davinci-003",
-				"prompt": query + ", Provide answer in medical/health context.",
-				"temperature": 0,
-				"max_tokens": 200
-			};
-
-			console.log(postData.prompt)
-
+			const googleApiKey = 'AIzaSyC3cuIyYnQR1zOPFv00jzy7tpulMNajR_0';
+			const googleapiUrl = `https://translation.googleapis.com/language/translate/v2?key=${googleApiKey}`;
+			const detect_url = `https://translation.googleapis.com/language/translate/v2/detect?key=${googleApiKey}`;
+			// console.log(postData.prompt)
+			// let openaiMessage = "";
+			// Language Detect API 	
 			try {
+				const detectData = {
+					q: query,
+				}
+				// console.log(googleData)
+				const response_detectAPI = await fetch(detect_url, {
+					method: 'POST',
+					body: JSON.stringify(detectData)
+				});
+				let detect_language = await response_detectAPI.json();
+				console.log(detect_language.data.detections[0][0].language)
+				setSourceLang(detect_language.data.detections[0][0].language)
+			} catch (error) {
+				console.error(error);
+			}
+			console.log("Language")
+			console.log(source_language)
+			// CONVERSION TO ENGLISH
+			// try {
+			// 	const googleData = {
+			// 		q: data,
+			// 		source: source_language,
+			// 		target: "en",
+			// 		format: 'text',
+			// 		key: googleApiKey,
+			// 	}
+			// 	// console.log(googleData)
+			// 	const response_openAI = await fetch(googleapiUrl, {
+			// 		method: 'POST',
+			// 		body: JSON.stringify(googleData)
+			// 	});
+			// 	let english = await response_openAI.json();
+			// 	console.log(english.data.translations[0].translatedText)
+			// 	setEnglishData(english.data.translations[0].translatedText)
+			// } catch (error) {
+			// 	console.error(error);
+			// }
+			let message=''; 
+			//OPEN AI Call
+			try {
+				const postData = {
+					"model": "text-davinci-003",
+					"prompt": query + ", Provide answer in medical/health context.",
+					"temperature": 0,
+					"max_tokens": 200
+				};	
 				const response = await fetch(apiUrl, {
 					method: 'POST',
 					headers: {
@@ -52,8 +96,30 @@ const HealthInformationLanding = (props) => {
 					},
 					body: JSON.stringify(postData)
 				});
+				// console.log(response)
 				let result = await response.json();
+				message = result.choices[0].text
 				setData(result.choices[0].text)
+			} catch (error) {
+				console.error(error);
+			}
+
+			try {
+				const googleData = {
+					q: message,
+					source: "en",
+					target: "hi",
+					format: 'text',
+					key: googleApiKey,
+				}
+				// console.log(googleData)
+				const response_openAI = await fetch(googleapiUrl, {
+					method: 'POST',
+					body: JSON.stringify(googleData)
+				});
+				let resp_openAI = await response_openAI.json();
+				// console.log(resp_openAI.data.translations[0].translatedText)
+				setTData(resp_openAI.data.translations[0].translatedText)
 			} catch (error) {
 				console.error(error);
 			}
@@ -68,6 +134,7 @@ const HealthInformationLanding = (props) => {
 		if (query) {
 			toggleTable();
 			console.log(data);
+			console.log(translated_data)
 		}
 	};
 
